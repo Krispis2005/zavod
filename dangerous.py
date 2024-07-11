2b55a770d37128ade6b70e1c7b3e33166efda7b6
Krispis <stepanovaks05@mail.ru>
2024-07-11 14:14:10 +0500

    dangerous

--- dangerous.py
+++ dangerous.py
@@ -3,18 +3,19 @@ import time
 import datetime
 
 time.sleep(30)
-def dangerous_situation (dt, long, people) :
+def dangerous_situation () :
     dt = datetime.datetime.now()
     long = random.uniform(0.1, 35)
     people = random.randint (1,10)
     a = random.random()
-    with open('logs.txt') as f:
+    stroka = str(dt) +' ' + str(long) +' ' + str(people)
     if a <= 0.4 :
-        return f.write.append(dt, long, people)
-    return "Ложное срабатывание"
+        with open('logs.txt', 'a') as f:
+            f.write(stroka)
+        return True
+    return False
 
-
-
+dangerous_situation()
 
 
 
